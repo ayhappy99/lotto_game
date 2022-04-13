1b912cc51d6d1df9d88a75ce81ae354d26f6044c
parkseonga <ayhappy99@naver.com>
2022-04-13 18:59:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lotto_game/game_select.java
@@ -0,0 +1,14 @@
+package com.example.lotto_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class game_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_select);
+    }
+}
