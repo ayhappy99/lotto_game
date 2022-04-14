cc2e1a420a2b27119692a934c972f790b934c05a
janghn <gkslcjstksla@gamil.com>
2022-04-14 16:36:48 +0900

    secont commit

--- app/src/main/java/com/example/lotto_game/MainActivity.java
+++ app/src/main/java/com/example/lotto_game/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lotto_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button go_game = findViewById(R.id.go_game);
+        Button lucky_num = findViewById(R.id.lucky_num);
+        Button ran_num = findViewById(R.id.ran_num);
+
+        go_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),game_select.class);
+                startActivity(intent);
+            }
+        });
+
+        lucky_num.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),lucky_num.class);
+                startActivity(intent);
+            }
+        });
+
+        ran_num.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),random.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/lotto_game/game_select.java
+++ app/src/main/java/com/example/lotto_game/game_select.java
@@ -2,7 +2,10 @@ package com.example.lotto_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class game_select extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class game_select extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_select);
+
+        Button card_game = findViewById(R.id.card_game);
+        Button roulette_game = findViewById(R.id.roulette_game);
+
+        card_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),card.class);
+                startActivity(intent);
+            }
+        });
+
+        roulette_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),roulette.class);
+                startActivity(intent);
+            }
+        });
     }
 }
